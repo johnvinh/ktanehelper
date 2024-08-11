6dcc6810a399c45081748bae6c53fb492f521f23
John Vinh <john.michael.vinh2@gmail.com>
2024-08-11 12:15:38 -0400

    Implement basic complicated wires logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,82 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+    let parallelPort = false;
+    let twoOrMoreBatteries = false;
+    let serialNumberIsEven = false;
+    let red = false;
+    let blue = false;
+    let light = false;
+    let star = false;
+
+    $: result = calculateResult(parallelPort, twoOrMoreBatteries, serialNumberIsEven, red, blue, light, star);
+
+    function calculateResult(parallelPort: boolean, twoOrMoreBatteries: boolean, serialNumberIsEven: boolean, red: boolean, blue: boolean, light: boolean, star: boolean) {
+        if (red && blue && star && light) {
+            return "DO NOT CUT";
+        } else if (red && blue && star && !light && parallelPort) {
+            return "CUT";
+        } else if (red && blue && !star && light && serialNumberIsEven) {
+            return "CUT";
+        } else if (red && blue && !star && !light && serialNumberIsEven) {
+            return "CUT";
+        } else if (red && !blue && star && light && twoOrMoreBatteries) {
+            return "CUT";
+        } else if (red && !blue && star && !light) {
+            return "CUT";
+        } else if (red && !blue && !star && light && twoOrMoreBatteries) {
+            return "CUT";
+        } else if (red && !blue && !star && !light && serialNumberIsEven) {
+            return "CUT";
+        } else if (!red && blue && star && light && parallelPort) {
+            return "CUT";
+        } else if (!red && blue && star && !light) {
+            return "DO NOT CUT";
+        } else if (!red && blue && !star && light && parallelPort) {
+            return "CUT";
+        } else if (!red && blue && !star && !light && serialNumberIsEven) {
+            return "CUT";
+        } else if (!red && !blue && star && light && twoOrMoreBatteries) {
+            return "CUT";
+        } else if (!red && !blue && star && !light) {
+            return "CUT";
+        } else if (!red && !blue && !star && light) {
+            return "DO NOT CUT";
+        } else if (!red && !blue && !star && !light) {
+            return "CUT";
+        }
+    }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<main>
+    <h1>Complicated Wires</h1>
+    <div>
+        <label for="parallelPort">Parallel Port</label>
+        <input type="checkbox" bind:checked={parallelPort} id="parallelPort">
+    </div>
+    <div>
+        <label for="twoOrMoreBatteries">Two or More Batteries</label>
+        <input type="checkbox" bind:checked={twoOrMoreBatteries} id="twoOrMoreBatteries">
+    </div>
+    <div>
+        <label for="serialNumberIsEven">Serial Number is Even</label>
+        <input type="checkbox" bind:checked={serialNumberIsEven} id="serialNumberIsEven">
+    </div>
+    <div>
+        <label for="red">Red</label>
+        <input type="checkbox" bind:checked={red} id="red">
+    </div>
+    <div>
+        <label for="blue">Blue</label>
+        <input type="checkbox" bind:checked={blue} id="blue">
+    </div>
+    <div>
+        <label for="light">Light</label>
+        <input type="checkbox" bind:checked={light} id="light">
+    </div>
+    <div>
+        <label for="star">Star</label>
+        <input type="checkbox" bind:checked={star} id="star">
+    </div>
+    <strong id="result">
+        {result}
+    </strong>
+</main>

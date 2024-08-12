25c06095bb69185789c143bddcf0823aca4c13a9
John Vinh <john.michael.vinh2@gmail.com>
2024-08-12 13:05:59 -0400

    Change to Sakura CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
-	import Header from './Header.svelte';
-	import '../mvp.css';
+	import '../sakura-dark.css';
 </script>
 
 <div class="app">
@@ -10,38 +9,5 @@
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>

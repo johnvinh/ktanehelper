711042953a742ddfcf17ff2468cbdc3ec75924ca
John Vinh <john.michael.vinh2@gmail.com>
2024-08-12 13:28:31 -0400

    Switch to 100% and display elements side-by-side

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script>
-	import '../sakura-dark.css';
+    import '../sakura-dark.css';
 </script>
 
 <div class="app">
-	<main>
-		<slot />
-	</main>
+    <main>
+        <slot />
+    </main>
 </div>
 
 <style>
-
+    main {
+        display: flex;
+        flex-direction: row;
+        gap: 2em;
+    }
 </style>
